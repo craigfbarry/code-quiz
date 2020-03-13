ccdb8a0b03ac3ce62405df667ca365a8400781f4
craigfbarry <craigfbarry@gmail.com>
2020-03-13 16:49:49 +1030

    Set up question array of objects and a for loop for now.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// Declaration of Variables to be used
 
+var secondsLeft = 10;
+var quizQuestions = [
+        { question: "?", answers: {a:"a",b:"b",c:"c",d:"d"}, correctAnswer :"a"},
+        { question: "?", answers: {a:"a",b:"b",c:"c",d:"d"}, correctAnswer :"b"},
+        { question: "?", answers: {a:"a",b:"b",c:"c",d:"d"}, correctAnswer :"c"},
+];
 
-var secondsLeft = 60;
+// A function for the quiz timer
 
 function timer(){
     interval = setInterval(function(){
@@ -14,7 +21,22 @@ function timer(){
         }
 
     },1000);
-
 }
 
+
+
 timer();
+
+//Setting up a loop to go through the question, using for loop for now but will change to forEach
+
+for (i=0;i<quizQuestions.length;i++){
+    console.log(quizQuestions[i]);
+    $("#quiz").text("Question " + i + ":");
+    $("#quiz").append();
+
+
+
+
+
+
+}
