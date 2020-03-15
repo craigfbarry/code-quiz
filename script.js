e20bc5a6636b7d22de4c67b0c6d2816d0e1af866
craigfbarry <craigfbarry@gmail.com>
2020-03-15 22:24:42 +1030

    Added jQuery source and set the timer to reduce on wrong answer

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Declaration of Variables to be used
 
-var secondsLeft = 10;
+var secondsLeft = 30;
 var questionCount = 0;
 var quizQuestions = [
         { question: "Who invented jquery?", answers: {a:"John Resig",b:"Bill Gates",c:"Mark Zuckerberg",d:"Elon Musk"}, correctAnswer :"a"},
         { question: "?", answers: {a:"a",b:"b",c:"c",d:"d"}, correctAnswer :"b"},
-        { question: "?", answers: {a:"a",b:"b",c:"c",d:"d"}, correctAnswer :"c"},
+        { question: "?", answers: {a:"a",b:"",c:"c",d:"d"}, correctAnswer :"c"},
 ];
 
 // A function for the quiz timer
@@ -13,6 +13,7 @@ $(document).ready(function(){
 
 
 $("#startButton").click(function startTimer(){
+    $("#startButton").remove();
     interval = setInterval(function(){
         secondsLeft--;
         //console.log(secondsLeft);
@@ -31,61 +32,74 @@ $("#startButton").click(function startTimer(){
 //Setting up a loop to go through the question, using for loop for now but will change to forEach
 
 
-//Add the question div
-$.each(quizQuestions, function displayQuiz(i){
+//Begin Quiz loop
+$("#startButton").click(function startQuiz(){
+    
+    //Add the question div
+    $.each(quizQuestions, function displayQuiz(i){
     
 
-    if (questionCount===i){
-    console.log("count " + questionCount);
-    console.log("count i " + i);
+        if (questionCount===i)
+        {
+        console.log("count " + questionCount);
+        console.log("count i " + i);
+    
+        let quizQuestion = $("<div>");  
+        quizQuestion.addClass("question row m-5").html("Question " + (i+1)+ " : " + quizQuestions[i].question);
+        $("#quiz").append(quizQuestion);
+        
+        
+    //Loop to set up the 4 answer buttons
+        $.each(quizQuestions[i].answers, function(j, answerValue){
+            let quizAnswer = $("<button>");
+            quizAnswer.attr("answer-letter",j);
+            quizAnswer.addClass("answer row m-5").html(answerValue);
+            $("#quiz").append($(quizAnswer));
+            }
+        );
 
-    let quizQuestion = $("<div>");  
-    quizQuestion.addClass("question row m-5").html("Question " + (i+1)+ " : " + quizQuestions[i].question);
-    $("#quiz").append(quizQuestion);
-    
-    
-//Loop to set up the 4 answer buttons
-    $.each(quizQuestions[i].answers, function(j, answerValue){
-        let quizAnswer = $("<button>");
-        quizAnswer.attr("answer-letter",j);
-        quizAnswer.addClass("answer row m-5").html(answerValue);
-        $("#quiz").append($(quizAnswer));
+
         }
-    );
+
+        
+
     }
+    );
+
+
     
-
-
-} );
-
 
     $(".answer").click(function checkAnswer(){
-        //var k=questionCount;
+        
         let result = $("<div>");
         let selection = ($(this).attr("answer-letter"));
         if (selection === quizQuestions[questionCount].correctAnswer) {
             result.html("Correct");
             result.appendTo("#quiz");
            
-        
-           //$("#quiz").empty();
+           $("#quiz").fadeOut(2000);
+           //$("#quiz").delay(5000).empty();
            questionCount++;
            console.log(questionCount);
+           
             }
         else if (selection !== quizQuestions[questionCount].correctAnswer) {
             result.html("Incorrect");
             result.appendTo("#quiz");  
+            secondsLeft = secondsLeft - 10;
             
-            //$("#quiz").empty();
+            $("#quiz").fadeOut(2000);
+            //$("#quiz").delay(5000).empty();
             questionCount++;
             console.log(questionCount);
-            
-             }         
+             }     
     });
-
     
 
-//} );
+
+
+})
+
 
 
 
