1f58b148bb089eccdd4a2596beb900a3ecd9922f
craigfbarry <craigfbarry@gmail.com>
2020-03-15 11:49:16 +1030

    Set up buttns and question with jquery.

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
 var secondsLeft = 10;
 var quizQuestions = [
-        { question: "?", answers: {a:"a",b:"b",c:"c",d:"d"}, correctAnswer :"a"},
+        { question: "Who invented jquery?", answers: {a:"John Resig",b:"Bill Gates",c:"Mark Zuckerberg",d:"Elon Musk"}, correctAnswer :"a"},
         { question: "?", answers: {a:"a",b:"b",c:"c",d:"d"}, correctAnswer :"b"},
         { question: "?", answers: {a:"a",b:"b",c:"c",d:"d"}, correctAnswer :"c"},
 ];
 
 // A function for the quiz timer
 
-function timer(){
+function startTimer(){
     interval = setInterval(function(){
         secondsLeft--;
         console.log(secondsLeft);
@@ -25,18 +25,35 @@ function timer(){
 
 
 
-timer();
+startTimer();
 
 //Setting up a loop to go through the question, using for loop for now but will change to forEach
 
-for (i=0;i<quizQuestions.length;i++){
-    console.log(quizQuestions[i]);
-    $("#quiz").text("Question " + i + ":");
-    $("#quiz").append();
+    let quizQuestion = $("<div>");
+    quizQuestion.addClass("question row").html("Question " + 1 + " : " + quizQuestions[0].question);
+    $("#quiz").append(quizQuestion);
+    
+    
+    let quizAnswer = $("<button>");
+    quizAnswer.addClass("answer row").html("a : " + quizQuestions[0].answers.a);
+    $("#quiz").append($(quizAnswer));
+    
+
+    let quizAnswer2 = $("<button>");
+    quizAnswer2.addClass("answer row").html("b : " + quizQuestions[0].answers.b);
+    $("#quiz").append($(quizAnswer2));
+
+    let quizAnswer3 = $("<button>");
+    quizAnswer3.addClass("answer row").html("c : " + quizQuestions[0].answers.c);
+    $("#quiz").append($(quizAnswer3));
+
+    let quizAnswer4 = $("<button>");
+    quizAnswer4.addClass("answer row").html("d : " + quizQuestions[0].answers.d);
+    $("#quiz").append($(quizAnswer4));
 
 
 
 
 
 
-}
+
